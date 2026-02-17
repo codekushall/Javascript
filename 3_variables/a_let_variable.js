196e73dc725a3bebecb2d561a9effe622a609853
codekushall <shresthakushal913@gmail.com>
2026-02-17 22:01:17 +0545

    variables naming learned

--- 3_variables/a_let_variable.js
+++ 3_variables/a_let_variable.js
@@ -4,10 +4,15 @@ let age;
 console.log(age); //undefined datatypes
 
 
+
 let age1 = 24;
 console.log(age1); // value is assigned to the age variable
 age1 = 30; // resigning the value of age1
 console.log(age1);
 
+
+// The age2 and Age has different values and its displays the value according to the values it has been assigned.
+let age2 = 90;
 let Age = 40;
+console.log(age2);
 console.log(Age);
---
+++ 3_variables/b_variable.js
@@ -0,0 +1,16 @@
+// valid  variable names
+let age;
+let _value;
+let $number;
+
+// Invalid variable names
+// let 1name;
+// let !age;
+// let @value;
+
+// camelCaseExamples   In cameCase it teaches us how to properly name the variable.
+let firstClassStudents;
+let sanothimiCampus;
+let namasteNepalCleaning;
+
+// In camelCase the first letter is lowercase and subsequent word starts with uppercase as per the above examples.
