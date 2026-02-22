45f0355f60bbb926c9d7b43a7e2e0655ccad8479
codekushall <shresthakushal913@gmail.com>
2026-02-22 21:51:10 +0545

    Console.log uses

---
+++ 6_console.log/a_use_of_console_log.js
@@ -0,0 +1,9 @@
+console.log("Bye, Bye"); // Prints Bye, Bye
+ 
+let name = "Kushal";
+console.log(name); // Prints Kushal
+
+console.log("Hello" + ' ' + name); // Pirnts Hello Kushal
+
+let age = 20;
+console.log("Name:", name, "Age:", age); // comma can also be used to pass multiple calues in console.log()
